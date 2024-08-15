ae4adff7adbce2e539399c25ab0320a2b32380f5
Unchanted <omkkdwivedi@gmail.com>
2024-08-15 12:47:02 +0530

    handled click events, mine generationg

--- _game.py
+++ _game.py
@@ -81,3 +81,50 @@ class Game(View):
                     if self.btnList.exists(spot):
                         self.btnClick(spot)
         self.checkWin()
+
+    def flag(self, c):
+        b = self.btnList.getBtn(c)
+        mb = self.btnList.getMSBtn(c)
+        if not mb.check:
+            mb.flagged = not mb.flagged
+            b.config(bg="yellow" if mb.flagged else "#cccccc")
+
+    def generateMines(self, sideW, sideH, count):
+        while count > 0:
+            rndm = Coord(random.randint(0, sideW), random.randint(0, sideH))
+            if rndm not in self.mineCoordList:
+                self.mineCoordList.append(rndm)
+                count -= 1
+
+    def createTiles(self, sideW, sideH):
+        for i in range(sideH):
+            for j in range(sideW):
+                c = Coord(j, i)
+                b = Button(self.screen, bg="#cccccc", width=2, height=1)
+                b.bind('<Button-1>', lambda event, c=c: self.btnClick(c))
+                b.bind('<Button-3>', lambda event, c=c: self.flag(c))
+
+                m = MSButton(b, 'M' if c in self.mineCoordList else '-', c)
+                m.show()
+                self.btnList.add(m)
+
+        for i in range(sideH):
+            for j in range(sideW):
+                c = Coord(j, i)
+                if self.btnList.get(c) == '-':
+                    mine_count = self.getImmediateMineCount(c)
+                    self.btnList.setVal('NIL' if mine_count == 0 else mine_count, c)
+
+    def getDiff(self):
+        self.diff = 0
+        with open('_settings.txt', 'r') as file:
+            for line in file:
+                if "Difficulty:" in line:
+                    self.diff = int(line.split('\t')[1])
+
+    def setScreen(self):
+        self.getDiff()
+        settings = [(10, 9, 9), (40, 16, 16), (100, 16, 30)]
+        self.mineCt, self.width, self.height = settings[self.diff] if self.diff < len(settings) else settings[0]
+        self.generateMines(self.width, self.height, self.mineCt)
+        self.createTiles(self.width, self.height)
